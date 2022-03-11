0ca0d577938e451b3ed5affa2775df90faad9f3a
itamar <itamar.moa@gmail.com>
2022-03-11 05:15:15 +0200

    removed unnecessary try catch

--- lambda/lib/getAllQuotes.js
+++ lambda/lib/getAllQuotes.js
@@ -2,10 +2,6 @@ const axios = require('axios');
 const externalUrl = process.env.EXTERNAL_URL;
 
 module.exports = async () => {
-  try {
-    const response = await axios.get(externalUrl);
-    return response?.data || [];
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await axios.get(externalUrl);
+  return response?.data;
 };
